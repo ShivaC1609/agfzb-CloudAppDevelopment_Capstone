26ec54d0e14dbaaa192645e98dfad0a199d6ad90
asdrabarlacca <98231830+asdrabarlacca@users.noreply.github.com>
2023-11-06 13:19:33 +0100

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -9,7 +9,14 @@ from django.utils.timezone import now
 # - Description
 # - Any other fields you would like to include in car make model
 # - __str__ method to print a car make object
+class CarMake(models.Model):
+    name = models.CharField(null=False, max_length=100, default='car name')
+    description = models.TextField
+    producer = models.CharField
 
+    # Create a toString method for object string representation
+    def __str__(self):
+        return self.name + " produced by " + self.producer + ". Description: " + self.description
 
 # <HINT> Create a Car Model model `class CarModel(models.Model):`:
 # - Many-To-One relationship to Car Make model (One Car Make has many Car Models, using ForeignKey field)
@@ -19,6 +26,34 @@ from django.utils.timezone import now
 # - Year (DateField)
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
+class CarModel(models.Model):
+    car_make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=100)
+    dealer_id = models.IntegerField(primary_key=True, default=1)
+    year = models.DateField(default=now)
+
+    SEDAN = "Sedan"
+    SUV = "SUV"
+    WAGON = "Station wagon"
+    SPORT = "Sports car"
+    BIKE = "Motor bike"
+    SCOOTER = "Scooter"
+    OTHER = "Other"
+    CAR_CHOICES = [
+        (SEDAN, "Sedan"), 
+        (SUV, "SUV"),
+        (WAGON, "Station wagon"),
+        (SPORT, "Sports Car"),
+        (BIKE, "Motor bike"),
+        (SCOOTER, "Scooter"),
+        (OTHER, 'Other')]
+    car_type = models.CharField(null=False, max_length=15, choices=CAR_CHOICES, default=SEDAN)
+
+    def __str__(self):
+        "name : " + self.name + "," + \
+        "dealer : " + self.dealer_id + "," + \
+        "year : " + self.year + "," + \
+        "type : " + self.car_type
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
