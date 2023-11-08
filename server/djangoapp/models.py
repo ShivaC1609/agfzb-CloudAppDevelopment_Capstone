ef5d92f64ba16fcec030c1f9478dea9377ca9ce4
asdrabarlacca <98231830+asdrabarlacca@users.noreply.github.com>
2023-11-08 21:02:46 +0100

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -27,33 +27,60 @@ class CarMake(models.Model):
 # - Any other fields you would like to include in car model
 # - __str__ method to print a car make object
 class CarModel(models.Model):
-    car_make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
-    name = models.CharField(null=False, max_length=100)
-    dealer_id = models.IntegerField(primary_key=True, default=1)
+    id = models.IntegerField(default=1,primary_key=True)
+    name = models.CharField(null=False, max_length=100, default='Car')
+   
+    SEDAN = 'Sedan'
+    SUV = 'SUV'
+    WAGON = 'Wagon'
+    MINIVAN = 'Minivan'
+    CAR_TYPES = [
+        (SEDAN, 'Sedan'),
+        (SUV, 'SUV'),
+        (WAGON, 'Wagon'),
+        (MINIVAN, 'Minivan')
+    ]
+
+    type = models.CharField(
+        null=False,
+        max_length=50,
+        choices=CAR_TYPES,
+        default=SEDAN
+    )
+    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
     year = models.DateField(default=now)
 
-    SEDAN = "Sedan"
-    SUV = "SUV"
-    WAGON = "Station wagon"
-    SPORT = "Sports car"
-    BIKE = "Motor bike"
-    SCOOTER = "Scooter"
-    OTHER = "Other"
-    CAR_CHOICES = [
-        (SEDAN, "Sedan"), 
-        (SUV, "SUV"),
-        (WAGON, "Station wagon"),
-        (SPORT, "Sports Car"),
-        (BIKE, "Motor bike"),
-        (SCOOTER, "Scooter"),
-        (OTHER, 'Other')]
-    car_type = models.CharField(null=False, max_length=15, choices=CAR_CHOICES, default=SEDAN)
-
     def __str__(self):
-        "name : " + self.name + "," + \
-        "dealer : " + self.dealer_id + "," + \
-        "year : " + self.year + "," + \
-        "type : " + self.car_type
+        return "Name: " + self.name
+
+#class CarModel(models.Model):
+#   car_make = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+#    name = models.CharField(null=False, max_length=100)
+#    dealer_id = models.IntegerField(primary_key=True, default=1)
+#   year = models.DateField(default=now)
+#
+#    SEDAN = "Sedan"
+#    SUV = "SUV"
+#    WAGON = "Station wagon"
+#    SPORT = "Sports car"
+#    BIKE = "Motor bike"
+#    SCOOTER = "Scooter"
+#    OTHER = "Other"
+#    CAR_CHOICES = [
+#        (SEDAN, "Sedan"), 
+#        (SUV, "SUV"),
+#        (WAGON, "Station wagon"),
+#        (SPORT, "Sports Car"),
+#        (BIKE, "Motor bike"),
+#        (SCOOTER, "Scooter"),
+#        (OTHER, 'Other')]
+#    car_type = models.CharField(null=False, max_length=15, choices=CAR_CHOICES, default=SEDAN)
+#
+#   def __str__(self):
+#        "name : " + self.name + "," + \
+#        "dealer : " + self.dealer_id + "," + \
+#        "year : " + self.year + "," + \
+#        "type : " + self.car_type
 
 
 # <HINT> Create a plain Python class `CarDealer` to hold dealer data
