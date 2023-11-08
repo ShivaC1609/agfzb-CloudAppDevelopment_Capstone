70e74da2170020ea9a3fac22e78b37cf7d41f529
asdrabarlacca <98231830+asdrabarlacca@users.noreply.github.com>
2023-11-08 21:05:53 +0100

    Update admin.py

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -12,7 +12,7 @@ class CarModelInline(admin.StackedInline):
 
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'car_type', 'year')
+    list_display = ('name', 'type', 'year')
     list_filter = ['year']
     search_fields = ['name', 'type']
 
