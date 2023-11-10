b21c9c976ea4913ede9941775eb1fc8ec837e71b
ShivaC1609 <146365682+ShivaC1609@users.noreply.github.com>
2023-11-10 17:24:52 +0530

    Update urls.py

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from .views import static_template_view, about_us_view, contact_us_view
 from django.conf.urls.static import static
 from django.conf import settings
 from . import views
@@ -7,11 +8,14 @@ app_name = 'djangoapp'
 urlpatterns = [
     # route is a string contains a URL pattern
     # view refers to the view function
+    path(route='static/', view=static_template_view, name='static_template'),
     # name the URL
 
     # path for about view
+    path(route='about/', view=about_us_view, name='about_us'),
 
     # path for contact us view
+    path(route='contact/', view=contact_us_view, name='contact_us'),
 
     # path for registration
 
@@ -20,6 +24,7 @@ urlpatterns = [
     # path for logout
 
     path(route='', view=views.get_dealerships, name='index'),
+    
 
     # path for dealer reviews view
 
