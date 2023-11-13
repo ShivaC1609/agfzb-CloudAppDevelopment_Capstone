955be20ab667ace6aee92df04dd0bd9993f0664e
ShivaC1609 <146365682+ShivaC1609@users.noreply.github.com>
2023-11-13 10:09:50 +0530

    Update models.py

--- server/djangoapp/models.py
+++ server/djangoapp/models.py
@@ -1,51 +1,45 @@
 from django.db import models
 from django.utils.timezone import now
-
+import json
 
 # Create your models here.
 
-# <HINT> Create a Car Make model `class CarMake(models.Model)`:
-# - Name
-# - Description
-# - Any other fields you would like to include in car make model
-# - __str__ method to print a car make object
 class CarMake(models.Model):
-    name = models.CharField(null=False, max_length=100, default='car name')
-    description = models.TextField
-    producer = models.CharField
+    name = models.CharField(null=False, max_length=100)
+    description = models.CharField(max_length=500)
 
-    # Create a toString method for object string representation
     def __str__(self):
-        return "Name: " + self.name
+        return "Name: " + self.name + ", " + "Description: " + self.description
 
 class CarModel(models.Model):
+    carmake = models.ForeignKey(CarMake, null=True, on_delete=models.CASCADE)
+    name = models.CharField(null=False, max_length=100)
     id = models.IntegerField(default=1,primary_key=True)
-    name = models.CharField(null=False, max_length=100, default='Car')
-   
-    SEDAN = 'Sedan'
-    SUV = 'SUV'
-    WAGON = 'Wagon'
-    MINIVAN = 'Minivan'
-    CAR_TYPES = [
-        (SEDAN, 'Sedan'),
-        (SUV, 'SUV'),
-        (WAGON, 'Wagon'),
-        (MINIVAN, 'Minivan')
+
+    # Choices for the car_type field as tuples
+    TYPE_CHOICES = [
+        ('Sedan', "Sedan"),
+        ('SUV', "SUV"),
+        ('Wagon', "Wagon")
     ]
 
-    type = models.CharField(
+    car_type = models.CharField(
         null=False,
-        max_length=50,
-        choices=CAR_TYPES,
-        default=SEDAN
+        max_length=20,
+        choices=TYPE_CHOICES,
+        default='Sedan'
     )
-    make = models.ForeignKey(CarMake, on_delete=models.CASCADE)
-    year = models.DateField(default=now)
+
+    year = models.DateField()
 
     def __str__(self):
-        return "Name: " + self.name
+        return "Name: " + self.name + ", " \
+               "Carmake: " + str(self.carmake) + ", " \
+               "Type: " + self.car_type + ", " \
+               "Year: " + str(self.year)
 
 
+# <HINT> Create a plain Python class `CarDealer` to hold dealer data
 class CarDealer:
 
     def __init__(self, address, city, full_name, id, lat, long, short_name, st, zip):
@@ -71,4 +65,30 @@ class CarDealer:
     def __str__(self):
         return "Dealer name: " + self.full_name
 
+
 # <HINT> Create a plain Python class `DealerReview` to hold review data
+class DealerReview:
+
+    def __init__(self, dealership, name, purchase, review, purchase_date, car_make, car_model, car_year, sentiment, id):
+        self.dealership = dealership
+        self.name = name
+        self.purchase = purchase
+        self.review = review
+        self.purchase_date = purchase_date
+        self.car_make = car_make
+        self.car_model = car_model
+        self.car_year = car_year
+        self.sentiment = sentiment
+        self.id = id
+
+    def __str__(self):
+        return "Dealership: " + self.dealership + \
+               "Name: " + self.name + \
+               "Purchase: " + self.purchase + \
+               "Review: " + self.review + \
+               "Purchase date: " + str(self.purchase_date) + \
+               "Car Make: " + self.car_make + \
+               "Car Model: " + self.car_model + \
+               "Car Year: " + str(self.car_year) + \
+               "Sentiment: " + self.sentiment + \
+               "id: " + str(self.id)
